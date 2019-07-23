cb9a33747b3d46523ec0a71b0c79dbdbaa1c8fda
xlfdll <xlfdll@gmail.com>
2019-07-23 01:10:04 -0700

    Added storyboard and image assets

---
+++ FPGiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FPGiOS
+//
+//  Created by Xlfdll on 2019/06/30.
+//  Copyright © 2019 Xlfdll Workstation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
